380fa9d0cd3e7812d1acff525d09b4d0697d11e1
Ryan Migaud <rmigaud@asu.edu>
2023-02-11 19:37:26 -0800

    Spring Boot Security example implementation.

---
+++ src/test/java/demo/security/SecurityApplicationTests.java
@@ -0,0 +1,14 @@
+package demo.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert(true);
+	}
+
+}
